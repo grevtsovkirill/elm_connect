80f0f91c4b1ed85e16db176076ac434096fe442b
Kirill Grevtsov <grevtsovkirill@gmail.com>
2025-11-06 20:33:16 +0100

    temp and rpm

--- elm_connect.ino
+++ elm_connect.ino
@@ -1,158 +1,101 @@
 #include <WiFi.h>
-#include <SPI.h>
+#include <WiFiClient.h>
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 
 TFT_eSPI tft = TFT_eSPI();
+#define TFT_BG TFT_BLACK
+#define TFT_TEXT TFT_GREEN
 
-#define SCREEN_WIDTH  320
-#define SCREEN_HEIGHT 240
-
-// --- Touchscreen pins (CYD defaults) ---
-#define XPT2046_IRQ 36
-#define XPT2046_MOSI 32
-#define XPT2046_MISO 39
-#define XPT2046_CLK 25
-#define XPT2046_CS 33
-
-SPIClass touchscreenSPI = SPIClass(VSPI);
-XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
-
-// --- Button geometry ---
-#define BTN_W 220
-#define BTN_H 60
-#define BTN_RADIUS 10
-
-struct Button {
-  int x, y;
-  String label;
-};
-
-Button btnConnect = { (SCREEN_WIDTH - BTN_W) / 2, 60, "Connect to ELM327 Wi-Fi" };
-Button btnSettings = { (SCREEN_WIDTH - BTN_W) / 2, 150, "Settings" };
-
-// --- Network settings ---
 const char* obdSSID = "WiFi-OBDII";
 const char* obdPASS = "";
-const IPAddress obdIP(192, 168, 0, 10);   // common ELM327 Wi-Fi IP
-int obdPort = 35000; 
+IPAddress obdIP(192, 168, 0, 10);
+int obdPort = 35000;
 
-// --- Button draw helpers ---
-void drawButton(Button b, uint16_t color, uint16_t textColor) {
-  tft.fillRoundRect(b.x, b.y, BTN_W, BTN_H, BTN_RADIUS, color);
-  tft.drawRoundRect(b.x, b.y, BTN_W, BTN_H, BTN_RADIUS, TFT_BLACK);
-  tft.setTextColor(textColor, color);
-  tft.drawCentreString(b.label, SCREEN_WIDTH / 2, b.y + (BTN_H / 2) - 8, 2);
-}
+WiFiClient client;
 
-bool isTouched(Button b, int tx, int ty) {
-  return (tx >= b.x && tx <= b.x + BTN_W && ty >= b.y && ty <= b.y + BTN_H);
-}
-
-// --- Display message helper ---
-void showMessage(String msg, uint16_t color = TFT_BLACK) {
-  tft.fillRect(0, 0, SCREEN_WIDTH, 30, TFT_WHITE);
-  tft.setTextColor(color, TFT_WHITE);
-  tft.drawCentreString(msg, SCREEN_WIDTH / 2, 5, 2);
+void showMessage(String msg, uint16_t color = TFT_TEXT) {
+  tft.fillScreen(TFT_BG);
+  tft.setTextColor(color, TFT_BG);
+  tft.drawCentreString(msg, 160, 120, 2);
   Serial.println(msg);
 }
 
-// --- Wi-Fi connect + ping ---
-void connectAndPing() {
-  showMessage("Connecting to ELM327 Wi-Fi...");
-  WiFi.disconnect(true);
+String sendCommand(String cmd, int wait = 500) {
+  client.print(cmd + "\r");
+  delay(wait);
+  String resp = "";
+  while (client.available()) {
+    resp += (char)client.read();
+  }
+  resp.trim();
+  Serial.println("> " + cmd);
+  Serial.println("< " + resp);
+  return resp;
+}
+
+void setup() {
+  Serial.begin(115200);
+  tft.init();
+  tft.setRotation(1);
+  showMessage("Connecting Wi-Fi...");
+
   WiFi.mode(WIFI_STA);
-
-  // Attempt open network connection first
-  Serial.println("Trying open Wi-Fi connection...");
-  WiFi.begin(obdSSID);
-
-  unsigned long startAttempt = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttempt < 10000) {
+  WiFi.begin(obdSSID, obdPASS);
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println();
+  Serial.println("Connected! IP: " + WiFi.localIP().toString());
+  showMessage("Wi-Fi connected!");
 
-  // If open network failed and we have a password, try WPA
-  if (WiFi.status() != WL_CONNECTED && strlen(obdPASS) > 0) {
-    Serial.println("Retrying with password...");
-    WiFi.begin(obdSSID, obdPASS);
-    startAttempt = millis();
-    while (WiFi.status() != WL_CONNECTED && millis() - startAttempt < 10000) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println();
+  if (!client.connect(obdIP, obdPort)) {
+    showMessage("ELM327 connect failed!", TFT_RED);
+    while (1);
   }
+  showMessage("ELM327 connected!");
 
-  if (WiFi.status() == WL_CONNECTED) {
-    IPAddress localIP = WiFi.localIP();
-    showMessage("Connected! IP: " + localIP.toString(), TFT_BLUE);
-    delay(1000);
-    showMessage("Testing OBDII link...", TFT_BLACK);
+  // Initialize ELM327
+  sendCommand("ATZ", 1000);
+  sendCommand("ATE0");
+  sendCommand("ATL0");
+  sendCommand("ATS0");
+  sendCommand("ATH0");
+  sendCommand("ATSP0");  // auto protocol
 
-    // --- Minimal "ping" replacement ---
-    WiFiClient client;
-    if (client.connect(obdIP, obdPort)) {   // ELM327 usually on port 23
-      showMessage("ELM327 reachable!", TFT_GREEN);
-      Serial.println("Connected to ELM327 successfully!");
-      client.stop();
-    } else {
-      showMessage("Cannot reach ELM327.", TFT_RED);
-      Serial.println("Failed to connect to ELM327 .");
-    }
-  } else {
-    showMessage("Wi-Fi connection failed!", TFT_RED);
-    Serial.println("Wi-Fi connection failed!");
-  }
-
-  delay(3000);
-  showMessage("Ready");
+  showMessage("Reading OBD...");
+  delay(1000);
 }
 
-
-
-// --- Setup ---
-void setup() {
-  Serial.begin(115200);
-  delay(500);
-
-  // Touch init
-  touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
-  touchscreen.begin(touchscreenSPI);
-  touchscreen.setRotation(1);
-
-  // Display init
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_WHITE);
-  tft.drawCentreString("CYD Wi-Fi ELM327 Interface", SCREEN_WIDTH / 2, 10, 2);
-  drawButton(btnConnect, TFT_CYAN, TFT_BLACK);
-  drawButton(btnSettings, TFT_YELLOW, TFT_BLACK);
-
-  showMessage("Touch any button...");
-}
-
-// --- Main loop ---
 void loop() {
-  if (touchscreen.tirqTouched() && touchscreen.touched()) {
-    TS_Point p = touchscreen.getPoint();
-    int x = map(p.x, 200, 3700, 1, SCREEN_WIDTH);
-    int y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
-
-    if (isTouched(btnConnect, x, y)) {
-      drawButton(btnConnect, TFT_BLUE, TFT_WHITE);
-      connectAndPing();
-      drawButton(btnConnect, TFT_CYAN, TFT_BLACK);
-    } else if (isTouched(btnSettings, x, y)) {
-      drawButton(btnSettings, TFT_ORANGE, TFT_WHITE);
-      showMessage("Settings (placeholder)", TFT_BLACK);
-      delay(1000);
-      drawButton(btnSettings, TFT_YELLOW, TFT_BLACK);
-      showMessage("Ready");
-    }
-
-    delay(200);
+  // Request RPM (PID 010C)
+  String rpmResp = sendCommand("010C");
+  int rpm = -1;
+  int idx = rpmResp.indexOf("410C");
+  if (idx >= 0 && rpmResp.length() >= idx + 8) {
+    String A_str = rpmResp.substring(idx + 4, idx + 6);
+    String B_str = rpmResp.substring(idx + 6, idx + 8);
+    int A = strtol(A_str.c_str(), NULL, 16);
+    int B = strtol(B_str.c_str(), NULL, 16);
+    rpm = ((A * 256) + B) / 4;
   }
+
+  // Request coolant temperature (PID 0105)
+  String tempResp = sendCommand("0105");
+  int temp = -1;
+  idx = tempResp.indexOf("4105");
+  if (idx >= 0 && tempResp.length() >= idx + 6) {
+    String A_str = tempResp.substring(idx + 4, idx + 6);
+    int A = strtol(A_str.c_str(), NULL, 16);
+    temp = A - 40;
+  }
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.drawCentreString("RPM: " + String(rpm), 160, 100, 4);
+  tft.drawCentreString("Temp: " + String(temp) + "C", 160, 140, 4);
+
+  delay(2000); // update every 2 seconds
 }
+
